fe399a5ce074b109ec355472e37fbf33634983a8
twiddle-bits <twiddlebits.ak@gmail.com>
2017-04-28 08:43:05 -0400

    Update tests-install.sh to call build with same CMAKE_BUILD_TYPE

--- tests-install.sh
+++ tests-install.sh
@@ -6,6 +6,14 @@ cd -
 
 set -e
 
+if [ -z "$CMAKE_BUILD_TYPE" ]; then
+    export CMAKE_BUILD_TYPE=Debug
+fi
+
+if [ -z "$BUILD_SHARED_LIBS" ]; then
+    export BUILD_SHARED_LIBS=1
+fi
+
 $root/build.sh
 cd $root/tests/install
 ./test.sh
